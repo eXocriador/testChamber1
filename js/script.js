cd0892275e217395d4eaceb387976ec37981be74
Oleh Tatarynov <exocriador@Olehs-MacBook-Pro.local>
2024-12-04 02:08:26 +0200

    update

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ const textEl = document.querySelector(".article-text");
 console.log(textEl.textContent);
 
 const titleEl = document.querySelector(".article-title");
-titleEl.textContent = 'Welcome to Bahamas!';
+titleEl.textContent = 'Welcome to Portugal!';
 console.log(titleEl.textContent);
 
 const allChange = document.querySelector(".article");
@@ -17,3 +17,12 @@ const hasActiveClassIsActive = link.classList.contains("is-active"); // true
 const hasActiveClassTitle = link.classList.contains("title"); // false
 console.log(hasActiveClassIsActive);
 console.log(hasActiveClassTitle);
+
+titleEl.classList.add("tyusha", "marta");
+console.log(titleEl.classList);
+// ["link", "is-active", "special", length: 3, value: "link is-active special"]
+const breadick = document.querySelector(".random-bread-box");
+breadick.classList.add("loh", "pidr", "skotinchick");
+console.log(breadick.classList);
+breadick.classList.remove("loh");
+console.log(breadick.classList);
